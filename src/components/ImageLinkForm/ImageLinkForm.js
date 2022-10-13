d3c8fc9785b812d2b154d94a6ee9833b6350ad38
Purple-Slayer <mutlu.tonguc@outlook.com>
2022-10-13 15:36:46 +0300

    example link change

--- src/components/ImageLinkForm/ImageLinkForm.js
+++ src/components/ImageLinkForm/ImageLinkForm.js
@@ -7,8 +7,8 @@ const ImageLinkForm = ({onInputChange,onButtonSubmit}) => {
         	<p className='f3'>
         		{'This Magic Brain will detect faces in your picture give it a try'}
         	</p>
-			<p className='f5'>
-        		{'example:https://m.media-amazon.com/images/M/MV5BNzg1MTUyNDYxOF5BMl5BanBnXkFtZTgwNTQ4MTE2MjE@._V1_UY1200_CR83,0,630,1200_AL_.jpg'}
+			<p className='f6'>
+        		{'(copy n paste)example: https://m.media-amazon.com/images/M/MV5BNzg1MTUyNDYxOF5BMl5BanBnXkFtZTgwNTQ4MTE2MjE@._V1_UY1200_CR83,0,630,1200_AL_.jpg'}
         	</p>
         	<div className='centerR'>
         	 <div className='form centerR pa4 br3 shadow-5' >
